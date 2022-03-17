22d5cf30fa11069b86bef96dcbe59aa7c9e77a22
James Demiraiakian <james.armen@gmail.com>
2022-03-16 17:03:35 -0700

    wrote tests for cats resource. I wrote this once going function by function. Now Im gonna go file by file per resource and see which workflow works better for me. But I will still  be working in a vertical type flow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('04-backend-hand-of-resources routes', () => {
   beforeEach(() => {
@@ -24,5 +25,46 @@ describe('04-backend-hand-of-resources routes', () => {
       id: expect.any(Number),
       ...expected,
     });
+  });
+
+  test('fetch all cats', async () => {
+    const expected = await Cat.findAll();
+    const res = await request(app).get('/api/v1/cats');
+
+    expect(res.body).toEqual(expected);
+  });
+
+  test('fetch only one (1) cat by id', async () => {
+    const expected = await Cat.findById(3);
+    const res = await app.get(`/api/v1/cats/${expected.id}`);
+
+    expect(res.body).toEqual({ ...expected });
+  });
+
+  test('return a 404 if try to pass in a counterfeit cat', async () => {
+    const res = await request(app).get('/api/v1/cats/counterfeit-cat');
+
+    expect(res.status).toEqual(404);
+  });
+
+  test('update a cat by its id', async () => {
+    const expected = {
+      id: expect.any(Number),
+      name: 'Test Cat',
+      age: 1234,
+      coat: 'Cat Coat Colors have been changed',
+    };
+    const res = await request(app)
+      .patch('/api/v1/cats/3')
+      .send({ coat: 'Cat Coat Colors have been changed' });
+
+    expect(res.body).toEqual(expected);
+  });
+
+  test('delete the test cat', async () => {
+    const expected = await Cat.findById(3);
+    const res = await request(app).delete(`/api/v1/cats/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
   });
 });
